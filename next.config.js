aca245126187613a4eb4051626384878434b8970
aphuus <aphuus@utu.fi>
2024-06-17 11:24:45 +0300

    publish

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
-if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
-}
-
-const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
-);
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
     remotePatterns: [
       {
-        protocol: protocol.slice(0, -1),
-        hostname,
-        port,
-        pathname: `${pathname}/**`,
+        protocol: "https",
+        hostname: "wordpress.aleksihuusko.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com",
+        port: "",
       },
     ],
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Container from "../components/container";
 import MoreStories from "../components/more-stories";
 import HeroPost from "../components/hero-post";
@@ -30,6 +32,7 @@ export default function Index({ allPosts: { edges }, preview }) {
           />
         )}
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        <SpeedInsights />
       </Container>
     </Layout>
   );

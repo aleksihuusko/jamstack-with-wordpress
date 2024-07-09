e319c0861c6e48a619524751f472fd016fd95ea6
aphuus <aphuus@utu.fi>
2024-07-09 09:47:30 +0300

    fix next-sitemap

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -3,6 +3,12 @@
 const config = {
   siteUrl: "https://jamstack.aleksihuusko.com",
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: "./public",
+  sitemapFilename: "sitemap.xml",
+  robotsTxtOptions: {
+    output: "robots.txt",
+  },
 };
 
 export default config;
